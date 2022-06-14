87b08e238910dacfe9724e1a808caa33f7febc13
Raees <raees4963@gmail.com>
2022-06-14 12:50:39 +0500

    did

--- Assingments/Percentage Calclator/java.js
+++ Assingments/Percentage Calclator/java.js
@@ -1,14 +1,33 @@
 // alert ("hello bhai");
 function myFunction() {
+
     var isl = document.getElementById('islamiat').value;
+
     var mth = document.getElementById('math').value;
+
     var eng = document.getElementById('english').value;
+
     var com = document.getElementById('computer').value;
+
+
     var urd = document.getElementById('urdu').value;
+
+
     var totalMark = 500;
-    var obtainMark = (isl + mth + eng + com + urd );
-    var percentage = (obtainMark * 100 ) / totalMark;
+// var p =  ;
+
+    var obtainMark = parseFloat(isl) + parseFloat(mth) + parseFloat(com) + parseFloat(urd) + parseFloat(eng);
+
+
+    
+    
+    var percentage = (obtainMark / totalMark) *100  ;
+    
+    alert(percentage) 
+
     console.log(percentage);
+
+
 var percentage = document.getElementById('percentage').value = percentage
 
 }

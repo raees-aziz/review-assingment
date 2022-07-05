8a1a677af84e7f5dbe6e701ce6608bf2365be783
Raees <raees4963@gmail.com>
2022-07-05 14:03:28 +0500

    counter

---
+++ Assingments/Counter/script.js
@@ -0,0 +1,48 @@
+var counter = document.querySelector('.counter')
+var count = 0 ;
+
+function myfunction(button)   {
+    if (button == plus) {
+        count += 1;
+    }
+    else if (button == minus){
+        count -= 1;
+    }else if (button == reset){
+        count = 0 ;
+    }
+    counter.innerHTML = count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function min(button) {
+// if (  )  {
+
+// }
+
+// }
+
+// function res(button) {
+// if (button== reset) {
+// count=0;
+// }
+    
+// }
+
+// function plus() {
+    
+// }
